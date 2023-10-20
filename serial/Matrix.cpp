c26ceea1281369ca69b94fea6c9e09a5c0725af0
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-20 20:26:01 +0300

    Add project files.

---
+++ serial/Matrix.cpp
@@ -0,0 +1,162 @@
+#include "Matrix.h"
+
+Matrix::Matrix(const size_t& size)
+{
+	outputWide = defaultOutputWide;
+	submatrix_index = 0;
+
+	this->width = size;
+	this->height = size;
+
+	values = new double[size * size] {0};
+	this->delete_on_release = true;
+}
+
+Matrix::Matrix(const size_t& width, const size_t& height)
+{
+	outputWide = defaultOutputWide;
+	submatrix_index = 0;
+
+	this->width = width;
+	this->height = height;
+
+	values = new double[width * height] {0};
+	delete_on_release = true;
+}
+
+Matrix::Matrix(double* values, const size_t& width, const size_t& height, const bool& delete_on_release)
+{
+	outputWide = defaultOutputWide;
+	submatrix_index = 0;
+
+	this->width = width;
+	this->height = height;
+
+	this->values = values;
+	this->delete_on_release = delete_on_release;
+}
+
+Matrix::~Matrix()
+{
+	if (delete_on_release)
+	{
+		delete[] values;
+	}
+}
+
+std::string Matrix::to_string() const
+{
+	std::stringstream string;
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+			string << std::setw(outputWide) << std::fixed << std::setprecision(4) << values[i * width + j];
+		}
+		string << std::setw(0) << std::endl;
+	}
+	return string.str();
+}
+
+size_t Matrix::get_width() const
+{
+	return width;
+}
+
+size_t Matrix::get_height() const
+{
+	return height;
+}
+
+size_t Matrix::get_submatrix_index() const
+{
+	return submatrix_index;
+}
+
+double* Matrix::get_values() const
+{
+	return values;
+}
+
+void Matrix::set_submutrix_index(const size_t& submatrix_index)
+{
+	this->submatrix_index = submatrix_index;
+}
+
+void Matrix::set_output_wide(const size_t& outputWide)
+{
+	this->outputWide = outputWide;
+}
+
+bool Matrix::operator==(const Matrix& other)
+{
+	if (this->width != other.width && this->height != other.height)
+	{
+		return false;
+	}
+
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+			if (values[i * width + j] != other.values[i * width + j])
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+void Matrix::dummy_data_initialization()
+{
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+			values[i * width + j] = (double)i;
+		}
+	}
+}
+
+void Matrix::random_data_initialization()
+{
+	srand(clock());
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+			values[i * width + j] = rand() / 1000.0;
+		}
+	}
+}
+
+Matrix operator*(const Matrix& left, const Matrix& right)
+{
+	if (left.width != right.height)
+	{
+		throw std::invalid_argument("Left matrix width not equal right matrix height!");
+	}
+	Matrix result(right.width, left.height);
+
+	for (int i = 0; i < left.height; i++)
+	{
+		for (int j = 0; j < right.width; j++)
+		{
+			for (int k = 0; k < right.height; k++)
+			{
+				result.values[i * left.width + j] += left.values[i * left.width + k] * right.values[k * right.width + j];
+			}
+		}
+	}
+
+	return result;
+}
+
+std::ostream& operator<<(std::ostream& out, const Matrix& matrix)
+{
+	out << matrix.to_string();
+
+	return out;
+}
---
+++ serial/Matrix.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+class Matrix
+{
+public:
+	Matrix(const size_t& size);
+	Matrix(const size_t& width, const size_t& height);
+	Matrix(double* values, const size_t& width, const size_t& height, const bool& delete_on_release = false);
+	~Matrix();
+
+	void dummy_data_initialization();
+	void random_data_initialization();
+
+	std::string to_string() const;
+	size_t get_width() const;
+	size_t get_height() const;
+	size_t get_submatrix_index() const;
+	double* get_values() const;
+
+	void set_submutrix_index(const size_t& submatrix_index);
+	void set_output_wide(const size_t& outputWide);
+
+	bool operator==(const Matrix& other);
+	friend Matrix operator*(const Matrix& left, const Matrix& right);
+
+	friend std::ostream& operator<< (std::ostream& out, const Matrix& matrix);
+
+private:
+	size_t height;
+	size_t width;
+	size_t submatrix_index;
+	double* values;
+
+	bool delete_on_release;
+
+	size_t outputWide;
+	const static size_t defaultOutputWide = 7;
+};
